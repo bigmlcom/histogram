9b59139870232fa70dec86241400d56de6cd09dd
Adam Ashenfelter <ashenfad@gmail.com>
2011-12-27 02:47:14 -0800

    Improves quality of heatmap merges

--- src/main/java/com/bigml/histogram/HeatMap.java
+++ src/main/java/com/bigml/histogram/HeatMap.java
@@ -1,22 +1,25 @@
 package com.bigml.histogram;
 
+import java.util.Collection;
+
 public class HeatMap {
-  
+
   public HeatMap() {
     this(128, 128);
   }
-  
+
   public HeatMap(int xHistSize, int yHistSize) {
     _hist = new Histogram<SimpleHistogramTarget>(xHistSize);
+    _mainHistSize = xHistSize;
     _targetHistSize = yHistSize;
   }
-  
+
   public HeatMap insert(double x, double y) {
     SimpleHistogramTarget target = new SimpleHistogramTarget(_targetHistSize, y);
     _hist.insert(new Bin<SimpleHistogramTarget>(x, 1, target));
     return this;
   }
-  
+
   public HeatMap insert(Number x, Number y) {
     return insert(x.doubleValue(), y.doubleValue());
   }
@@ -29,15 +32,46 @@ public class HeatMap {
       return xResult.getTargetSum().getTarget().density(y);
     }
   }
-  
+
   public double density(Number x, Number y) {
     return density(x.doubleValue(), y.doubleValue());
   }
-  
+
   public Histogram<SimpleHistogramTarget> getHistogram() {
     return _hist;
   }
-  
+
+  public static HeatMap merge(Collection<HeatMap> heatMaps) {
+    int maxMainHist = -Integer.MAX_VALUE;
+    int maxTargetHist = -Integer.MAX_VALUE;
+    for (HeatMap hm : heatMaps) {
+      maxMainHist = Math.max(maxMainHist, hm._mainHistSize);
+      maxTargetHist = Math.max(maxTargetHist, hm._targetHistSize);
+    }
+
+    Histogram<SimpleHistogramTarget> tempHist =
+            new Histogram<SimpleHistogramTarget>(heatMaps.size() * maxMainHist);
+
+    Histogram<SimpleHistogramTarget> newHist = null;
+    try {
+      for (HeatMap hm : heatMaps) {
+        tempHist.merge(hm.getHistogram());
+      }
+
+      newHist = new Histogram<SimpleHistogramTarget>(maxMainHist);
+      newHist.merge(tempHist);
+    } catch (MixedInsertException ex) {
+    }
+
+    return new HeatMap(maxMainHist, maxTargetHist, newHist);
+  }
+
+  private HeatMap(int mainHistSize, int targetHistSize, Histogram<SimpleHistogramTarget> hist) {
+    _hist = hist;
+    _mainHistSize = mainHistSize;
+    _targetHistSize = targetHistSize;
+  }
   private final Histogram<SimpleHistogramTarget> _hist;
+  private final int _mainHistSize;
   private final int _targetHistSize;
 }
