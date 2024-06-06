ada60dbdf305f1b458aa9968bfc4fb363b68a27e
jao <jao@gnu.org>
2024-06-06 15:51:09 +0100

    Dependencies update, including latest clojure 1.11.3
    Fixes #69

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject bigml/histogram "4.1.4"
+(defproject bigml/histogram "5.0.0"
   :description "Streaming histograms for Clojure/Java"
   :min-lein-version "2.0.0"
   :url "https://github.com/bigmlcom/histogram"
@@ -8,10 +8,16 @@
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :javac-options ["-source" "1.8" "-target" "1.8"]
-  :aliases {"lint" ["do" "check," "eastwood"]
-            "distcheck" ["do" "clean," "lint," "test"]}
-  :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]
-                   :dependencies [[incanter/incanter-core "1.9.1"]
-                                  [incanter/incanter-charts "1.9.1"]]}}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :aliases {"lint"  ^{:doc "Run the Eastwood linter on source files"}
+            ["do" "check," "eastwood"]
+            "update" ^{:doc "Update dependencies in project.clj using ancient"}
+            ["ancient" "upgrade" ":no-tests" ":check-clojure" ":all"]
+            "distcheck" ^{:doc "Prepare a dist running all tests and lints"}
+            ["do" "clean," "lint," "test"]}
+  :profiles {:dev {:plugins [[jonase/eastwood "1.4.2"]
+                             [lein-ancient "1.0.0-RC3"]]
+                   :dependencies [[incanter/incanter-core "1.9.3"]
+                                  [incanter/incanter-charts "1.9.3"]]}}
+  :dependencies [[org.clojure/clojure "1.11.3"]
+                 [org.clojure/core.rrb-vector "0.2.0"]
                  [com.googlecode.json-simple/json-simple "1.1.1"]])
